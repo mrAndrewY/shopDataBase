452871a9a36980347e9947ceaad8689a73cbd9ce
kenyetta <kenyetta@student.21-school.ru>
2024-07-04 19:57:49 +0300

    Исправлена таблица клиент

--- database.sql
+++ database.sql
@@ -57,11 +57,16 @@ create table if not exists shop.client (
     id serial primary key,
     firstname varchar(40) not null,
     lastname varchar(40) not null,
-    phone varchar(12) not null,
-    phone_extra varchar(12),
-    email varchar(40) not null,
+    phone varchar(18) not null,
+    phone_extra varchar(18),
+    email varchar(40) not null unique,
     address varchar(200),
-    unique (firstname, lastname)
+    create_at timestamp not null,
+    updated_at timestamp,
+    notes varchar(1000),
+    last_order_date date,
+    total_orders int,
+    total_spent money
 );
 
 create table if not exists shop.employee (
